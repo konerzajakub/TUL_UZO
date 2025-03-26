1fac75f6ab7c844583cf9b672ce1e22de50985da
ddeejjvviidd <david.salek@tul.cz>
2025-03-26 11:16:29 +0100

    cv07 snad vse

--- cv07/main.py
+++ cv07/main.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
 from pathlib import Path
-from collections import deque
+
 
 
 def get_histogram(image, smoothing):
@@ -19,6 +18,7 @@ def get_histogram(image, smoothing):
     return smoothed
 
 
+
 def get_threshold(array):
     # vypocet hodnoty prahu pro segmentaci z histogramu
     differences = (array[1:-1] < array[0:-2]) * (array[1:-1] < array[2:])
@@ -29,36 +29,35 @@ def get_threshold(array):
     return threshold
 
 
-def bfs_coloring(image):
-    # barveni prohledavani do sirky
+
+def colors(image):
     rows, cols = image.shape
     visited = np.zeros_like(image, dtype=bool)
     colors = np.zeros_like(image, dtype=np.uint8)
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    
+
     color_count = 0
     for i in range(rows):
         for j in range(cols):
             if not visited[i, j] and image[i, j] == 1:
                 color_count += 1
-                queue = deque([(i, j)])
+                stack = [(i, j)]
                 visited[i, j] = True
                 colors[i, j] = color_count
-                
-                while queue:
-                    current_x, current_y = queue.popleft()
-                    
+
+                while stack:
+                    current_x, current_y = stack.pop()
+
                     for dx, dy in directions:
                         new_x, new_y = current_x + dx, current_y + dy
-                        
-                        #if is_valid(new_x, new_y):
+
                         if 0 <= new_x < rows and 0 <= new_y < cols:
                             if not visited[new_x, new_y] and image[new_x, new_y] == 1:
-                                queue.append((new_x, new_y))
+                                stack.append((new_x, new_y))
                                 visited[new_x, new_y] = True
                                 colors[new_x, new_y] = color_count
-    
     return colors
+
 
 
 def mass_center(image):
@@ -78,14 +77,17 @@ def mass_center(image):
     
     return points
 
-def get_region_values(image, points):
+
+
+def coin_values(image, points):
     values = []
     
     for point in points:
         x, y = point[0], point[1]
         region_number = image[y][x]
-        region_pixels = np.argwhere(image == region_number)
-        number_of_pixels = len(region_pixels)
+
+        region_pixels = np.argwhere(image == region_number) # binarni mapa stejnych pixelu
+        number_of_pixels = len(region_pixels) # pocet pixelu pro porovnani minci
         
         # pokud ma vic jak 4000 pixelu, tak je to petikoruna
         if number_of_pixels > 4000:
@@ -96,6 +98,7 @@ def get_region_values(image, points):
         values.append({"point": point, "value": value})
     
     return values
+
 
 
 def main():
@@ -109,52 +112,64 @@ def main():
     g = 255 - ((green * 255) / (red + green + blue))
     g_hist = get_histogram(g, 10)
     
+    # prah pro segmentaci
     threshold = get_threshold(g_hist)
     
+    # segmentace 
     segmented_image = np.where(g < threshold, 0, 1)
 
-    regions = bfs_coloring(segmented_image)
+    # ziskani oblasti
+    regions = colors(segmented_image)
     
+    # teziste oblasti
     points = mass_center(regions)
     
-    coin_values = get_region_values(regions, points)
+    # rozeznani mince
+    values = coin_values(regions, points)
 
-    # vypis hodnot
-    for value in coin_values:
-        print(value)
+
+    fig, axes = plt.subplots(2, 4, figsize=(14, 8))
     
-    fig, axes = plt.subplots(2, 4, figsize=(12, 6))
-    
+    # puvodni obrazek
     axes[0, 0].imshow(img)
     axes[0, 0].set_title("Img")
     
+    # zelena slozka
     axes[0, 1].imshow(g, cmap="gray")
     axes[0, 1].set_title("Zelena")
     
+    # segmentace
     axes[0, 2].imshow(segmented_image, cmap="gray")
-    axes[0, 2].set_title("Segmentace obrázku")
+    axes[0, 2].set_title("Segmentace")
 
+    # histogram zelene
     axes[0, 3].plot(g_hist)
     #axes[0, 3].axvline(threshold, color="red")
     axes[0, 3].set_xlim(0, 255)
     axes[0, 3].set_ylim(0, 255)
-    axes[0, 3].set_title("Histogram zelené složky")
+    axes[0, 3].set_title("Green hist")
     
+    # vykresleni oblasti
     axes[1, 0].imshow(regions, cmap='jet')
-    axes[1, 0].set_title("Oblasti obrázku")
+    axes[1, 0].set_title("Oblasti")
     
+    #vykresleni tezist
     axes[1, 1].imshow(img)
     axes[1, 1].scatter(*zip(*points), marker="+", color="red")
-    axes[1, 1].set_title("Těžiště oblastí")
+    axes[1, 1].set_title("Teziste")
     
+    # vykresleni hodnot minci
     axes[1, 2].imshow(img)
-    for coin in coin_values:
+    for coin in values:
         x, y, value = coin.get("point")[0], coin.get("point")[1], coin.get("value")
         axes[1, 2].text(x, y, value, color="red", fontsize=16)
-    axes[1, 2].set_title("Hodnota oblastí")
+        print(f'x,y {{{x};{y}}}, mince {value}') # vypis do konzole
+    axes[1, 2].set_title("Hodnota oblasti")
     
     plt.tight_layout()
     plt.show()
+
+
 
 if __name__ == "__main__":
     main()
